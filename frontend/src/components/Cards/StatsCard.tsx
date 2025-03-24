a9cfc3eed708986c5f188cc4840f761283e1a4df
ggsoria <gonzalosoria.sg@gmail.com>
2025-03-24 13:32:27 -0300

    stat cards created

---
+++ frontend/src/components/Cards/StatsCard.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './StatCards.css'
+import { FaArrowUp } from "react-icons/fa6";
+import { FaArrowDown } from "react-icons/fa6";
+
+const statIcons = {
+    positive:<FaArrowUp size={38} color="rgba(0, 172, 79, 1)"/>,
+    negative:<FaArrowDown size={38} color="rgba(232, 35, 35, 1)"/>,
+}
+export const StatsCard = () => {
+  return (
+    <div className='statcards__container'>
+    <div className='statcard'>
+        <div className='statcard__info-container'>
+            <div className='statcard__title'>Valor positivo</div>        
+            <div className='statcard__value'>$700.000</div>
+        </div>
+        <div className='statcard__icon'>
+            {statIcons.positive}
+        </div>
+    </div>
+    <div className='statcard'>
+        <div className='statcard__info-container'>
+            <div className='statcard__title'>Valor positivo</div>        
+            <div className='statcard__value'>$700.000</div>
+        </div>
+        <div className='statcard__icon'>
+            {statIcons.positive}
+        </div>
+    </div>
+    <div className='statcard'>
+        <div className='statcard__info-container'>
+            <div className='statcard__title'>Valor positivo</div>        
+            <div className='statcard__value'>$700.000</div>
+        </div>
+        <div className='statcard__icon'>
+            {statIcons.negative}
+        </div>
+    </div>
+    <div className='statcard'>
+        <div className='statcard__info-container'>
+            <div className='statcard__title'>Valor positivo</div>        
+            <div className='statcard__value'>$700.000</div>
+        </div>
+        <div className='statcard__icon'>
+            {statIcons.negative}
+        </div>
+    </div>
+    </div>
+    
+  )
+}

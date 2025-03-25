3e1ac318a686753d84ce97e7ab7068e58b75109b
ggsoria <gonzalosoria.sg@gmail.com>
2025-03-24 22:54:19 -0300

    custom select added to chart

--- frontend/src/components/Chart/ChartTopBar.tsx
+++ frontend/src/components/Chart/ChartTopBar.tsx
@@ -1,29 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { PerformanceIndicator } from '../PerformanceIndicator/PerformanceIndicator';
+import { CustomSelect } from '../Utils/CustomSelect';
 
 export const ChartTopBar = () => {
-    const [dataRange, setDataRange] = useState("Anual");
-    const dateRanges = ["Anual", "Mensual", "Semanal", "Diario"];
-
   return (
     <div className='chart-top-bar__container'>
         <div className='chart-top-bar__title'>
-            <div>Grafico de evolucion</div>
-            {/* <div className='chart-top-bar__performance'> || +2.45%</div> */}
+            <div>Grafico finanzas</div>
             <PerformanceIndicator />
         </div>
         <div className='chart-top-bar__select'>
-            <select
-              id="dateRange"
-              value={dataRange}
-              onChange={(e) => setDataRange(e.target.value)}
-            >
-              <option value="" disabled>-- Seleccione una temporalidad --</option>
-              {dateRanges.map((dateRange) => (
-                <option key={dateRange} value={dateRange}>{dateRange}</option>
-              ))}
-            </select>
-        </div>    
+        <CustomSelect />
+        </div>
     </div>
   )
 }

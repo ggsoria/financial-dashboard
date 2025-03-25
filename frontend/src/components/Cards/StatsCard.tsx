095b3a9fd1fb8b10d37bf3c1fd66aa6d3dda2ef2
ggsoria <gonzalosoria.sg@gmail.com>
2025-03-25 12:12:34 -0300

    stats card updated with mocked data

--- frontend/src/components/Cards/StatsCard.tsx
+++ frontend/src/components/Cards/StatsCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './StatCards.css'
 import { FaArrowUp } from "react-icons/fa6";
 import { FaArrowDown } from "react-icons/fa6";
@@ -7,13 +7,14 @@ const statIcons = {
     positive:<FaArrowUp size={38} color="rgba(0, 172, 79, 1)"/>,
     negative:<FaArrowDown size={38} color="rgba(232, 35, 35, 1)"/>,
 }
+
 export const StatsCard = () => {
   return (
     <div className='statcards__container'>
     <div className='statcard'>
         <div className='statcard__info-container'>
             <div className='statcard__title'>Valor positivo</div>        
-            <div className='statcard__value'>$700.000</div>
+            <div className='statcard__value'>$700.000 ARS</div>
         </div>
         <div className='statcard__icon'>
             {statIcons.positive}
@@ -22,7 +23,7 @@ export const StatsCard = () => {
     <div className='statcard'>
         <div className='statcard__info-container'>
             <div className='statcard__title'>Valor positivo</div>        
-            <div className='statcard__value'>$700.000</div>
+            <div className='statcard__value'>$3.400 USD</div>
         </div>
         <div className='statcard__icon'>
             {statIcons.positive}
@@ -30,8 +31,8 @@ export const StatsCard = () => {
     </div>
     <div className='statcard'>
         <div className='statcard__info-container'>
-            <div className='statcard__title'>Valor positivo</div>        
-            <div className='statcard__value'>$700.000</div>
+            <div className='statcard__title'>Valor negativo</div>        
+            <div className='statcard__value'>$230.000 ARS</div>
         </div>
         <div className='statcard__icon'>
             {statIcons.negative}
@@ -39,8 +40,8 @@ export const StatsCard = () => {
     </div>
     <div className='statcard'>
         <div className='statcard__info-container'>
-            <div className='statcard__title'>Valor positivo</div>        
-            <div className='statcard__value'>$700.000</div>
+            <div className='statcard__title'>Valor negativo</div>        
+            <div className='statcard__value'>$1.200 USD</div>
         </div>
         <div className='statcard__icon'>
             {statIcons.negative}

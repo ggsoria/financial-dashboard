e99a7faf9349c677edf84e725554104e69ac609e
ggsoria <gonzalosoria.sg@gmail.com>
2025-03-24 22:55:23 -0300

    top bar responsive added to sidebar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './Sidebar.css'
 import { SidebarItems } from './SidebarItems'
-import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoMdNotificationsOutline, IoMdMenu } from "react-icons/io";
 import { MdLogout } from "react-icons/md";
 
 
@@ -12,28 +12,37 @@ export const Sidebar = () => {
     setActiveItem(itemName);
   }
 
-  return (
-    <div className='sidebar'>
-      <div className='sidebar__header'>
-        <div className='header__title'>|||| LOGO EMPRESA</div>
-        <IoMdNotificationsOutline size={24} color='white'/>
-      </div>
-      <ul className='sidebar__list'>        
-        {SidebarItems.map((item, key) => {
-          return(
-            <li className={activeItem === item.name ? 'list__row--active' : 'list__row'} key={key} onClick={() => handleItemClick(item.name)}> 
-              <div id='icon' className={activeItem === item.name ? 'row__icon--active' : 'row__icon'}>{item.icon}</div>
-              <div className={activeItem === item.name ? 'row__text--active' : 'row__text'}>{item.name}</div>
-            </li>
-          )
-        })}
-      </ul> 
-      <div className='sidebar__footer'>
-        <div className='footer__items'>
-          <div id='icon'><MdLogout size={24} color='white'/></div>
-          <div className='footer__text'>Cerrar sesion</div>
+    return (
+    <>
+    <div className="topbar">
+      <div className="topbar__logo">|||| LOGO EMPRESA</div>
+      <IoMdMenu size={30} color="rgba(0, 73, 146, 1)" className="topbar__menu-icon" />
+    </div>
+
+      <div className="sidebar">
+        <div className='sidebar__header'>
+          <div className='header__title'>|||| LOGO EMPRESA</div>
+          <IoMdNotificationsOutline size={24} color='white'/>
+        </div>
+
+        <ul className='sidebar__list'>        
+          {SidebarItems.map((item, key) => {
+            return(
+              <li className={activeItem === item.name ? 'list__row--active' : 'list__row'} key={key} onClick={() => handleItemClick(item.name)}> 
+                <div id='icon' className={activeItem === item.name ? 'row__icon--active' : 'row__icon'}>{item.icon}</div>
+                <div className={activeItem === item.name ? 'row__text--active' : 'row__text'}>{item.name}</div>
+              </li>
+            )
+          })}
+        </ul> 
+
+        <div className='sidebar__footer'>
+          <div className='footer__items'>
+            <div id='icon'><MdLogout size={24} color='white'/></div>
+            <div className='footer__text'>Cerrar sesion</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }

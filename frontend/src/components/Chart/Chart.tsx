74069a6470dd7b5ecc4404432ba5a1348c9806ef
ggsoria <gonzalosoria.sg@gmail.com>
2025-03-24 22:56:42 -0300

    responsive design, char with mobile status for styles

--- frontend/src/components/Chart/Chart.tsx
+++ frontend/src/components/Chart/Chart.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CartesianGrid, XAxis, YAxis, Tooltip, AreaChart, Area, ResponsiveContainer } from 'recharts';
 import './Chart.css'
 import { ChartTopBar } from './ChartTopBar';
@@ -14,27 +14,40 @@ const data = [
 ];
 
 export const Chart = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  
   return (
     <div className="chart__container">
         <ChartTopBar />
-        <ResponsiveContainer width='100%' height='80%'>
+        <ResponsiveContainer width='100%' height={isMobile ? '60%' : '80%'}>
             <AreaChart data={data} margin={{left: -20}}>
             <defs>
                 <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
                 <stop offset="1.43%" stopColor="rgba(172, 172, 172)" stopOpacity={0.8}/>
                 <stop offset="75%" stopColor="rgba(28, 104, 170)" stopOpacity={0}/>
                 </linearGradient>
-
             </defs>
-            <XAxis dataKey="name" axisLine={false} tickLine={false}/>
-            <YAxis axisLine={false} tickLine={false} />
-            <CartesianGrid vertical={false} strokeDasharray="4"/>
+            <XAxis dataKey="name" axisLine={false} tickLine={false}  hide={isMobile}/>
+            <YAxis axisLine={false} tickLine={false}  hide={isMobile}/>
+            <CartesianGrid vertical={false} strokeDasharray="4" horizontal={!isMobile}/>
             <Tooltip />
             <Area type="monotone" dataKey="uv" stroke="rgba(0, 118, 184, 1)" strokeWidth='3.75' fillOpacity={1} fill="url(#colorUv)" />
             {/* <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" /> */}
             </AreaChart>
         </ResponsiveContainer>
-    </div>
+        { isMobile && 
+        (<a href="#" style={{ color: "rgba(0, 73, 146, 1)", fontWeight: 500, fontSize: "22px", alignSelf: "center" }}> Ver detalle</a>)}
+        </div>
   )
 }
 
